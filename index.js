b2957639d076fe30a6b4cd09fbf38268e0f28f4f
shoxjahon527 <134792325+shoxjahon527@users.noreply.github.com>
2023-11-20 19:09:47 +0500

    todo

---
+++ index.js
@@ -0,0 +1,20 @@
+let inputBx = document.querySelector('#inputBx');
+let listlar = document.querySelector('#list');
+inputBx.addEventListener("keyup", function(event){
+    if (event.key == "Enter") {
+        addItem(this.value)
+        this.value = ""
+    }
+})
+let addItem = (inputBx) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `${inputBx}   <i>`;
+    listItem.addEventListener("click", function(){
+        this.classList.toggle('done')
+    })
+    listItem.querySelector("i").addEventListener("click",
+    function() {
+        listItem.remove();
+    })
+    listlar.appendChild(listItem)
+}
